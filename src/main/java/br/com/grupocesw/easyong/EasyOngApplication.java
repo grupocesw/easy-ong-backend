399083123e65077d5e869eddd1893f876acb0d58
Andrê Reges <dreeh.silva@hotmail.com>
2021-02-13 12:17:23 -0300

    Created Easy Ong Project

---
+++ src/main/java/br/com/grupocesw/easyong/EasyOngApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupocesw.easyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyOngApplication.class, args);
+	}
+
+}
