b27f77b3661acebf7029032acb514a6ce90936a7
Andrê Reges <dreeh.silva@hotmail.com>
2021-07-18 22:06:54 -0300

    Deleted NgoSlimV2ResponseDto

--- src/main/java/br/com/grupocesw/easyong/controllers/NgoV2Controller.java
+++ src/main/java/br/com/grupocesw/easyong/controllers/NgoV2Controller.java
@@ -1,7 +1,6 @@
 package br.com.grupocesw.easyong.controllers;
 
 import br.com.grupocesw.easyong.mappers.NgoV2Mapper;
-import br.com.grupocesw.easyong.response.dtos.NgoSlimV2ResponseDto;
 import br.com.grupocesw.easyong.response.dtos.NgoV2ResponseDto;
 import br.com.grupocesw.easyong.services.NgoService;
 import io.swagger.annotations.*;
@@ -19,28 +18,6 @@ import springfox.documentation.annotations.ApiIgnore;
 public class NgoV2Controller {
 
 	private final NgoService service;
-
-	@ApiOperation(value = "Return pageable list of NGOs High Lights by default 20 items and also you can also use name, description " +
-			"and social cause name filters")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Return list successfully"),
-			@ApiResponse(code = 500, message = "An exception was generated")
-	})
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "page", dataType = "integer", paramType = "query", value = "Results page you want to retrieve (0..N)"),
-			@ApiImplicitParam(name = "size", dataType = "integer", paramType = "query", value = "Number of records per page."),
-			@ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query", value = "Sorting criteria in the format: property(,asc|desc). "
-					+ "Default sort order is ascending. " + "Multiple sort criteria are supported.") })
-	@GetMapping("/high-lights")
-	public Page<NgoSlimV2ResponseDto> highLightsList(
-			@RequestParam(required = false) String filter,
-			@ApiIgnore final Pageable pageable) {
-
-		if (filter != null)
-			return NgoV2Mapper.INSTANCE.listToSlimResponseDto(service.findByActivatedWithFilter(filter, pageable));
-		else
-			return NgoV2Mapper.INSTANCE.listToSlimResponseDto(service.findByActivated(pageable));
-	}
 
 	@ApiOperation(value = "Return pageable list of NGOs by default 20 items and also you can also use name, description " +
 			"and social cause name filters")
@@ -73,17 +50,6 @@ public class NgoV2Controller {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<NgoV2ResponseDto> retrieve(@PathVariable Long id) {
 		return ResponseEntity.ok(NgoV2Mapper.INSTANCE.entityToResponseDto(service.retrieve(id)));
-	}
-
-	@ApiOperation(value = "Get one NGO High Lights")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Get one successfully"),
-			@ApiResponse(code = 404, message = "Resource not found"),
-			@ApiResponse(code = 500, message = "An exception was generated")
-	})
-	@GetMapping(value = "/high-lights/{id}")
-	public ResponseEntity<NgoSlimV2ResponseDto> highLightsRetrieve(@PathVariable Long id) {
-		return ResponseEntity.ok(NgoV2Mapper.INSTANCE.entityToSlimResponseDto(service.retrieve(id)));
 	}
 	 
 }
--- src/main/java/br/com/grupocesw/easyong/mappers/NgoV2Mapper.java
+++ src/main/java/br/com/grupocesw/easyong/mappers/NgoV2Mapper.java
@@ -3,7 +3,6 @@ package br.com.grupocesw.easyong.mappers;
 import br.com.grupocesw.easyong.entities.Ngo;
 import br.com.grupocesw.easyong.entities.SocialCause;
 import br.com.grupocesw.easyong.enums.ContactType;
-import br.com.grupocesw.easyong.response.dtos.NgoSlimV2ResponseDto;
 import br.com.grupocesw.easyong.response.dtos.NgoV2ResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -17,11 +16,6 @@ import java.util.stream.Collectors;
 public interface NgoV2Mapper {
     NgoV2Mapper INSTANCE = Mappers.getMapper(NgoV2Mapper.class);
 
-    default Set<NgoSlimV2ResponseDto> listToSlimResponseDtoSet(Set<Ngo> entities) {
-        return entities.stream()
-                .map(this::entityToSlimResponseDto)
-                .collect(Collectors.toSet());
-    }
 
     default Set<NgoV2ResponseDto> listToResponseDtoSet(Set<Ngo> entities) {
         return entities.stream()
@@ -29,25 +23,9 @@ public interface NgoV2Mapper {
                 .collect(Collectors.toSet());
     }
 
-    default Page<NgoSlimV2ResponseDto> listToSlimResponseDto(Page<Ngo> entities) {
-            return entities.map(this::entityToSlimResponseDto);
-    }
-
     default Page<NgoV2ResponseDto> listToResponseDto(Page<Ngo> entities) {
         return entities.map(this::entityToResponseDto);
     }
-
-    @Mapping(target = "title", expression = "java(entity.getName())")
-    @Mapping(target = "website", expression = "java(entity.getContacts()" +
-            ".stream()" +
-            ".filter(contact -> contact.getType().equals(ContactType.WEB_SITE))" +
-            ".findFirst().isPresent() ? entity.getContacts()" +
-            ".stream()" +
-            ".filter(contact -> contact.getType().equals(ContactType.WEB_SITE))" +
-            ".findFirst().get().getContent() : \"\")")
-    @Mapping(target = "url", expression = "java(entity.getPictures().stream().findFirst().isPresent() ?" +
-            "entity.getPictures().stream().findFirst().get().getUrl() : \"\")")
-    NgoSlimV2ResponseDto entityToSlimResponseDto(Ngo entity);
 
     @Mapping(target = "contact", expression = "java(SimpleContactMapper.INSTANCE.entityToResponseDto(entity))")
     @Mapping(target = "location", expression = "java(LocationMapper.INSTANCE.entityToResponseDto(entity.getAddress()))")
