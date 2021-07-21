9576dac7fb693f1007634753139bd097a0a51ae2
Andrê Reges <dreeh.silva@hotmail.com>
2021-07-21 20:33:00 -0300

    Add cors netlify url

--- src/main/java/br/com/grupocesw/easyong/configs/WebMvcConfig.java
+++ src/main/java/br/com/grupocesw/easyong/configs/WebMvcConfig.java
@@ -12,17 +12,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
-            .allowedOrigins("https://goofy-almeida-c15740.netlify.app")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(MAX_AGE_SECS);
-
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedOrigins(
+                    "*"
+//                "https://goofy-almeida-c15740.netlify.app",
+//                "http://localhost:3000"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(MAX_AGE_SECS);
