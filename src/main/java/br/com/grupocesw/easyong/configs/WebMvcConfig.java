b45be42dd763825a35ca125f0935f93941385c08
Andrê Reges <dreeh.silva@hotmail.com>
2021-07-21 20:41:04 -0300

    Add cors netlify url

--- src/main/java/br/com/grupocesw/easyong/configs/WebMvcConfig.java
+++ src/main/java/br/com/grupocesw/easyong/configs/WebMvcConfig.java
@@ -2,10 +2,12 @@ package br.com.grupocesw.easyong.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
@@ -13,12 +15,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                    "*"
-//                "https://goofy-almeida-c15740.netlify.app",
-//                "http://localhost:3000"
-            )
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:3000", "https://goofy-almeida-c15740.netlify.app")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(MAX_AGE_SECS);
